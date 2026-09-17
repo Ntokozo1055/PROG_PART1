f2304fccc8ac2195e5867371b9d7415f868b274b
Ntokozo1055 <ST10514083@rcconnect.edu.za>
2026-09-18 00:21:34 +0200

    check password and cell number

--- src/main/java/com/mycompany/javapart1/Login.java
+++ src/main/java/com/mycompany/javapart1/Login.java
@@ -19,6 +19,68 @@ public class Login {
     //Check username
     
     boolean checkUsername(String username){
-        return username.contains("_")&& username.length()<=5;
+        if(username.contains("_")&& username.length()<=5){
+            return true;
+        }else{
+            return false;
+        }
     }
+    
+    //Check password complexity
+    
+    boolean checkPasswordComplexity(String password){
+        boolean length=false;
+        boolean capital=false;
+        boolean number=false;
+        boolean specialChar=false;
+        
+        //first check the length
+        
+        if(password.length()>=8){
+            length=true;
+        }
+        
+        //use a while loop to check all the required characters
+        
+        int i=0;
+        while(i<password.length()){
+            char character=password.charAt(i);
+            
+            if(Character.isUpperCase(character)){
+                capital=true;
+            }else if(Character.isDigit(character)){
+                number=true;
+            }else if(!Character.isLetterOrDigit(character)){
+                specialChar=true;
+            }
+             i++;       
+        }
+        
+        if(length && number && specialChar){
+            return true;
+        }else{
+            return false;
+        }
+                
+    }
+    
+    //Check the cell phone number
+    
+    boolean checkCellPhoneNumber(String cellPhoneNum){
+        if(cellPhoneNum.startsWith("+27")&& cellPhoneNum.length()<=10){
+            
+            int i=1;
+            while(i<cellPhoneNum.length()){
+                if(!Character.isDigit(cellPhoneNum.charAt(i))){
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
 }
