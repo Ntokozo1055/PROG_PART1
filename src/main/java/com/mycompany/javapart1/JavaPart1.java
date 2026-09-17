b44e4af895f174cf87725d6e3651abd2da0268d9
Ntokozo1055 <ST10514083@rcconnect.edu.za>
2026-09-17 23:10:37 +0200

    first commit

---
+++ src/main/java/com/mycompany/javapart1/JavaPart1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javapart1;
+
+/**
+ *
+ * @author ntokozo
+ */
+public class JavaPart1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
